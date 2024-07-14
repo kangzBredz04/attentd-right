fcbac603805626083eaf87b9c1a4460de1de862f
wahyu <wabredz1234@gmail.com>
2024-07-15 01:29:31 +0700

    tambah beberapa rute untuk akses halaman

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,46 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import UserPage from "./pages/UserPage.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    // errorElement: <ErrorPageAdmin />,
+    children: [
+      {
+        path: "/admin",
+        // element: <DashboardAdmin />,
+      },
+      {
+        path: "/admin/user",
+        element: <UserPage />,
+      },
+      {
+        path: "/admin/profile",
+        // element: <MyAccountAdmin />,
+      },
+      {
+        path: "/admin/stock",
+        // element: <StockAdmin />,
+      },
+      {
+        path: "/admin/order",
+        // element: <OrderAdmin />,
+      },
+      {
+        path: "/admin/product",
+        // element: <ProductAdmin />,
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
