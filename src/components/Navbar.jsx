3e2bebb03196eb85126933bcc87abe87e3f90d8c
wahyu <wabredz1234@gmail.com>
2024-07-17 23:44:57 +0700

    tambah pop up ketika menekan ikon profile

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,17 +1,36 @@
+import { useState } from "react";
 import { FaUserCircle } from "react-icons/fa";
+import { FaRegUser } from "react-icons/fa6";
+import { RiLogoutBoxLine } from "react-icons/ri";
 
 export default function Navbar() {
+  const [modal, setModal] = useState(false);
   return (
     <header className="z-10 py-4 bg-white shadow-md">
       <div className="container flex items-center justify-between h-full px-6 mx-auto text-orange-500">
         <ul className="flex items-center w-fit ml-auto space-x-6">
           <li className="relative">
-            <button className="align-middle rounded-full focus:shadow-outline-purple focus:outline-none">
-              <FaUserCircle size={26} />
+            <button
+              onClick={() => setModal(!modal)}
+              className="align-middle rounded-full focus:shadow-outline-purple focus:outline-none"
+            >
+              <FaUserCircle size={32} />
             </button>
           </li>
         </ul>
       </div>
+      {modal && (
+        <div className="absolute bg-white border border-gray-300 right-7 top-14 py-2 w-48 px-3 rounded-lg flex flex-col gap-2 shadow-xl">
+          <div className="flex items-center gap-3 cursor-pointer">
+            <FaRegUser />
+            <p>Profile</p>
+          </div>
+          <div className="flex items-center gap-3 cursor-pointer">
+            <RiLogoutBoxLine />
+            <p>Log Out</p>
+          </div>
+        </div>
+      )}
     </header>
   );
 }
