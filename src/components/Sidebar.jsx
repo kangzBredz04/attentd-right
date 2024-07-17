0cb08a98d27ed96d7b994459c121eb8b256f7ef6
wahyu <wabredz1234@gmail.com>
2024-07-17 22:39:23 +0700

    pada bagian komponen location ditambah link untuk akses ke halaman location

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -67,7 +67,10 @@ export default function SideBar() {
             </Link>
           </li>
           <li className="relative px-6 py-3">
-            <Link className="inline-flex items-center w-full text-sm font-semibold transition-colors duration-150 hover:text-gray-800">
+            <Link
+              to={"/admin/location"}
+              className="inline-flex items-center w-full text-sm font-semibold transition-colors duration-150 hover:text-gray-800"
+            >
               <SlLocationPin size={20} />
               <span className="ml-4">Location</span>
             </Link>
