0ea36db3cc72bc9e945378d90079a7cd8deac550
wahyu <wabredz1234@gmail.com>
2024-07-18 00:20:32 +0700

    update tampilan halaman presence

--- src/pages/PresencePage.jsx
+++ src/pages/PresencePage.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 /* eslint-disable react/prop-types */
 import { useState } from "react";
 import {
@@ -7,62 +8,41 @@ import {
   FaAngleLeft,
 } from "react-icons/fa";
 
-const notificationData = [
+const presenceData = [
   {
     id: 1,
-    user: "super_admin",
-    type: "Dashboard",
-    message: "admin mengajukan pengajuan perubahan shift!",
+    name: "Tomo",
+    attendance_date: "26 June 2024",
+    check_in: "09:23:24",
+    check_out: "Belum melakukan absen pulang.",
   },
   {
     id: 2,
-    user: "user_test",
-    type: "Notification",
-    message: "Notification untuk semua users",
+    name: "Tomo",
+    attendance_date: "26 June 2024",
+    check_in: "11:23:24",
+    check_out: "Belum melakukan absen pulang.",
   },
   {
     id: 3,
-    user: "admin",
-    type: "Notification",
-    message: "Notification untuk semua users",
-  },
-  {
-    id: 4,
-    user: "super_admin",
-    type: "Notification",
-    message: "Notification untuk semua users",
+    name: "Super Admin",
+    attendance_date: "26 June 2024",
+    check_in: "15:11:24",
+    check_out: "15:13:24",
   },
 ];
 
 export default function PresencePage() {
   const [keyword, setKeyword] = useState("");
-  const [notifications, setNotifications] = useState(notificationData);
-  const [modal, setModal] = useState({ action: "", notification: null });
+  const [presences, setPresences] = useState(presenceData);
   const [entriesPerPage, setEntriesPerPage] = useState(10);
   const [currentPage, setCurrentPage] = useState(1);
 
-  const filter = notifications.filter(
-    (notification) =>
-      notification.user.toLowerCase().includes(keyword.toLowerCase()) ||
-      notification.type.toLowerCase().includes(keyword.toLowerCase()) ||
-      notification.message.toLowerCase().includes(keyword.toLowerCase())
+  const filter = presences.filter(
+    (presence) =>
+      presence.name.toLowerCase().includes(keyword.toLowerCase()) ||
+      presence.attendance_date.toLowerCase().includes(keyword.toLowerCase())
   );
-
-  const handleAddNotification = () => {
-    setModal({ action: "add", notification: null });
-  };
-
-  const handleSaveNotification = (notification) => {
-    if (modal.action === "edit") {
-      setNotifications(
-        notifications.map((n) => (n.id === notification.id ? notification : n))
-      );
-    } else {
-      notification.id = notifications.length + 1;
-      setNotifications([...notifications, notification]);
-    }
-    setModal({ action: "", user: null });
-  };
 
   const handleChangeEntriesPerPage = (e) => {
     setEntriesPerPage(Number(e.target.value));
@@ -76,7 +56,7 @@ export default function PresencePage() {
     setCurrentPage(newPage);
   };
 
-  const paginatedNotification = filter.slice(
+  const paginatedPresence = filter.slice(
     (currentPage - 1) * entriesPerPage,
     currentPage * entriesPerPage
   );
@@ -85,14 +65,8 @@ export default function PresencePage() {
       <div className="container grid px-6 mx-auto">
         <div className="my-6 flex justify-between items-center">
           <h2 className="text-2xl font-semibold text-gray-700">
-            Tabel Notification
+            Tabel Presence
           </h2>
-          <button
-            onClick={handleAddNotification}
-            className="add-button px-4 py-2 text-sm font-medium text-white transition-colors duration-150 bg-orange-500 border border-transparent rounded-lg active:bg-orange-500 hover:bg-orange-600 focus:outline-none focus:shadow-outline-purple"
-          >
-            Add Notification
-          </button>
         </div>
         <div className="w-full overflow-hidden rounded-lg shadow-xs">
           <div className="w-full overflow-x-auto bg-white p-4">
@@ -125,9 +99,10 @@ export default function PresencePage() {
                   <table className="w-full whitespace-no-wrap">
                     <colgroup>
                       <col className="w-[92.1979px]" />
-                      <col className="w-[161.135px]" />
-                      <col className="w-[150.438px]" />
-                      <col className="w-[518.229px]" />
+                      <col className="w-[149.5px]" />
+                      <col className="w-[211.469px]" />
+                      <col className="w-[132.177px]" />
+                      <col className="w-[336.656px]" />
                     </colgroup>
                     <thead>
                       <tr className="text-xs font-semibold tracking-wide text-left text-gray-600 uppercase border-b bg-gray-50">
@@ -135,30 +110,36 @@ export default function PresencePage() {
                           ID
                         </th>
                         <th className="cursor-pointer relative pr-[30px] text-center p-[10px] border-b-[1px] border-gray-600 font-bold">
-                          USER
+                          NAME
                         </th>
                         <th className="cursor-pointer relative pr-[30px] text-center p-[10px] border-b-[1px] border-gray-600 font-bold">
-                          TYPE
+                          ATTENDANCE DATE
                         </th>
                         <th className="cursor-pointer relative pr-[30px] text-center p-[10px] border-b-[1px] border-gray-600 font-bold">
-                          MESSAGE
+                          CHECH IN
+                        </th>
+                        <th className="cursor-pointer relative pr-[30px] text-center p-[10px] border-b-[1px] border-gray-600 font-bold">
+                          CHECH OUT
                         </th>
                       </tr>
                     </thead>
                     <tbody className="divide-y text-xs text-gray-600">
-                      {paginatedNotification.map((n, index) => (
+                      {paginatedPresence.map((p, index) => (
                         <tr key={index} className="h-10">
                           <td className="text-center py-[8px] px-[10px]">
                             {index + 1}
                           </td>
                           <td className="text-center py-[8px] px-[10px]">
-                            {n.user}
+                            {p.name}
                           </td>
                           <td className="text-center py-[8px] px-[10px]">
-                            {n.type}
+                            {p.attendance_date}
                           </td>
                           <td className="text-center py-[8px] px-[10px]">
-                            {n.message}
+                            {p.check_in}
+                          </td>
+                          <td className="text-center py-[8px] px-[10px]">
+                            {p.check_out}
                           </td>
                         </tr>
                       ))}
@@ -169,7 +150,7 @@ export default function PresencePage() {
               <div className="flex justify-between mx-10 my-5">
                 <div>
                   <p>
-                    Showing {paginatedNotification.length} of {filter.length}{" "}
+                    Showing {paginatedPresence.length} of {filter.length}{" "}
                     entries
                   </p>
                 </div>
@@ -215,105 +196,6 @@ export default function PresencePage() {
           </div>
         </div>
       </div>
-      {modal.action && (
-        <NotificationModal
-          action={modal.action}
-          notification={modal.notification}
-          onClose={() => setModal({ action: "", notification: null })}
-          onSave={handleSaveNotification}
-        />
-      )}
     </main>
-  );
-}
-
-function NotificationModal({ action, notification, onClose, onSave }) {
-  const [formData, setFormData] = useState(
-    notification || {
-      user: "",
-      type: "",
-      message: "",
-    }
-  );
-
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setFormData((prevData) => ({
-      ...prevData,
-      [name]: value,
-    }));
-  };
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    onSave(formData);
-  };
-
-  return (
-    <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50">
-      <div className="bg-white p-6 rounded-lg shadow-lg w-[90%] md:w-[50%]">
-        <h2 className="text-xl font-bold mb-4">
-          {action === "add"
-            ? "Add Notification"
-            : action === "edit"
-            ? "Edit Notification"
-            : "View Notification"}
-        </h2>
-        <form onSubmit={handleSubmit}>
-          <div className="grid grid-cols-1 md:grid-cols-1 gap-4">
-            <select
-              name="user"
-              value={formData.user}
-              onChange={handleChange}
-              disabled={action === "show"}
-              className="p-2 border rounded"
-            >
-              <option value="semua user">Semua user</option>
-              <option value="Super Admin">Super Admin</option>
-              <option value="Admin">Admin</option>
-              <option value="Client">Client</option>
-            </select>
-            <select
-              name="type"
-              value={formData.type}
-              onChange={handleChange}
-              disabled={action === "show"}
-              className="p-2 border rounded"
-            >
-              <option value="" disabled>
-                Type
-              </option>
-              <option value="Dashboard">Dashboard</option>
-              <option value="Notification">Notification</option>
-            </select>
-            <input
-              name="message"
-              value={formData.message}
-              onChange={handleChange}
-              disabled={action === "show"}
-              placeholder="Message"
-              className="p-2 border rounded h-32"
-            />
-          </div>
-          <div className="mt-4 flex justify-end gap-4">
-            <button
-              type="button"
-              onClick={onClose}
-              className="px-4 py-2 bg-gray-300 text-black rounded"
-            >
-              Cancel
-            </button>
-            {action !== "show" && (
-              <button
-                type="submit"
-                className="px-4 py-2 bg-blue-500 text-white rounded"
-              >
-                Save
-              </button>
-            )}
-          </div>
-        </form>
-      </div>
-    </div>
   );
 }
